9262e591e1de0cdbba2aea04963ec176a7602b58
kylin <33998180+ceoifung@users.noreply.github.com>
2022-04-29 16:40:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+<!--
+ * @Author: Ceoifung
+ * @Date: 2022-04-29 16:27:18
+ * @LastEditTime: 2022-04-29 16:38:39
+ * @LastEditors: Ceoifung
+ * @Description: 
+ * XiaoRGEEK All Rights Reserved, Powered by Ceoifung
+-->
+# electron-vue-builder
+
+> A electron-vue builder template with latest electron version
+
+electron: ^13.0.0版本的electron-vue模板，electron-builder打包
+
+#### Build Setup
+
+``` bash
+# install dependencies
+yarn
+
+# serve with hot reload at localhost:9080
+yarn dev
+
+# build electron application for production
+yarn build
+
+# build unpack application
+yarn build:dir
+
+```
+
+---
+
+This project was generated with [electron-vue](https://github.com/SimulatedGREG/electron-vue)@[8d4ed60](https://github.com/SimulatedGREG/electron-vue/tree/8d4ed607d65300381a8f47d97923eb07832b1a9a) using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://simulatedgreg.gitbooks.io/electron-vue/content/index.html).
---
+++ src/main/index.js
@@ -0,0 +1,80 @@
+/*
+ * @Author: Ceoifung
+ * @Date: 2022-04-29 16:27:18
+ * @LastEditTime: 2022-04-29 16:33:13
+ * @LastEditors: Ceoifung
+ * @Description: main.js
+ * XiaoRGEEK All Rights Reserved, Powered by Ceoifung
+ */
+import { app, BrowserWindow } from 'electron'
+import '../renderer/store'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+// if you want load native module like serialport set follow code
+// app.allowRendererProcessReuse = false 
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
